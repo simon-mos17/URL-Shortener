2e96329310d27b2e5bdbef007b6868d92b88f42b
simon-mos17 <simonmoser1@gmx.de>
2026-03-04 11:44:25 +0100

    Add Service

---
+++ src/main/java/org/example/shortener/ShortLinkService.java
@@ -0,0 +1,59 @@
+package org.example.shortener;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ShortLinkService {
+
+    private final ShortLinkRepository repo;
+    private static final String base62 = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    public ShortLinkService(ShortLinkRepository repo) {
+        this.repo = repo;
+    }
+
+    public String getShortedLink(String originalLink) {
+
+        Optional<ShortLink> optional = repo.findByOriginalLink(originalLink);
+
+        if (optional.isPresent()) {
+            return optional.get().getShortedLink();
+        }
+        return null;
+    }
+
+    public String getOriginalLink(String shortedLink) {
+
+        Optional<ShortLink> optional = repo.findByShortedLink(shortedLink);
+
+        if (optional.isPresent()) {
+            return optional.get().getOriginalLink();
+        }
+        return null;
+    }
+
+    public ShortLink createShortLink(String originalLink) {
+
+        Optional<ShortLink> existing = repo.findByOriginalLink(originalLink);
+        if (existing.isPresent()) {
+            return existing.get();
+        }
+
+        ShortLink shortLink = repo.save(new ShortLink(originalLink, null));
+        long n = shortLink.getId();
+
+        String shortedLink = "";
+
+        while (n > 0) {
+            int mod = (int) (n % 62);
+            shortedLink = base62.charAt(mod) + shortedLink;
+            n = n / 62;
+        }
+
+        shortLink.setShortedLink(shortedLink);
+
+        return repo.save(shortLink); // update
+    }
+}
